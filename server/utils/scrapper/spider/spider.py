a54452d3d0ce8d8eca146bf7133bb6ed27a8264c
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-06-26 20:24:05 +0700

    feat:(go to detail steps and scrape)

--- server/utils/scrapper/spider/spider.py
+++ server/utils/scrapper/spider/spider.py
@@ -4,17 +4,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-import time
+import json
 
 class DynamicSpider(scrapy.Spider):
     name = 'dynamic_spider'
-    allowed_domains = ['lpse.pu.go.id']
-    start_urls = ['https://lpse.bogorkab.go.id/eproc4/lelang']
+    allowed_domains = ['lpse.jakarta.go.id']
+    start_urls = ['https://lpse.jakarta.go.id/eproc4']
     items = []
-    
+
     def __init__(self):
-        super(DynamicSpider, self).__init__()
+        super().__init__()
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=chrome_options)
@@ -22,39 +21,36 @@ class DynamicSpider(scrapy.Spider):
     def parse(self, response):
         self.driver.get(response.url)
 
-        # Locate and interact with the selection input
-        select_xpath = "/html/body/div[6]/div/div/div/div/div[1]/div[1]/div[1]/div/label/select"
-
         WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, select_xpath))
+            EC.presence_of_element_located((By.XPATH, "//*[@id='main']/div/div/div[3]/div/div/div[2]/table/tbody/tr[contains(@class, 'Jasa_Konsultansi_Badan_Usaha_Non_Konstruksi')]//a"))
         )
 
-        select_element = self.driver.find_element(By.XPATH, select_xpath)
-        select = Select(select_element)
-        select.select_by_index(2)  # Assuming options are 0-based index then pick 100
+        types = [
+            'Pengadaan_Barang', 'Jasa_Konsultansi_Badan_Usaha_Non_Konstruksi', 'Pekerjaan_Konstruksi',
+            'Jasa_Lainnya', 'Jasa_Konsultansi_Perorangan_Non_Konstruksi', 'Jasa_Konsultansi_Badan_Usaha_Konstruksi',
+            'Jasa_Konsultansi_Perorangan_Konstruksi', 'Pekerjaan_Konstruksi_Terintegrasi'
+        ]
+        anchors = []
 
-        # Wait for 5 seconds to let the page load dynamically
-        time.sleep(5)
-
-        WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[6]/div/div/div/div/div[1]/div[2]/div/table/tbody/tr"))
-        )
-
-        rows = self.driver.find_elements(By.XPATH, "/html/body/div[6]/div/div/div/div/div[1]/div[2]/div/table/tbody/tr")
+        for type in types:
+            data = self.driver.find_elements(By.XPATH, f"//*[@id='main']/div/div/div[3]/div/div/div[2]/table/tbody/tr[contains(@class, '{type}')]//a")
+            if data:
+                anchors.extend(data)
+        
         main_window = self.driver.current_window_handle
 
-        for row in rows:
-            kode_tender = row.find_element(By.XPATH, "./td[1]").text
-            detail_url = f"https://lpse.bogorkab.go.id/eproc4/lelang/{kode_tender}/pengumumanlelang"
+        for anchor in anchors:
+            detail_url = anchor.get_attribute('href')
             self.driver.execute_script(f"window.open('{detail_url}', '_blank');")
-            self.driver.switch_to.window(self.driver.window_handles[1])
+            WebDriverWait(self.driver, 10).until(lambda d: len(d.window_handles) > 1)
+            self.driver.switch_to.window(self.driver.window_handles[-1])
             WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/table/tbody/tr[1]/td/strong"))
             )
 
             item = self.extract_detail_page()
-            self.items.append(item)
 
+            self.items.append(item)
             self.driver.close()
             self.driver.switch_to.window(main_window)
 
@@ -83,6 +79,40 @@ class DynamicSpider(scrapy.Spider):
         item['lokasi_pengerjaan'] = get_text_or_default("//tr[th[contains(text(), 'Lokasi Pekerjaan')]]/td[1]/ul/li")
         item['syarat_kualifikasi'] = get_text_or_default("//tr[th[contains(text(), 'Syarat Kualifikasi')]]/td[1]")
         item['peserta_tender'] = get_text_or_default("//tr[th[contains(text(), 'Peserta Tender')]]/td[1]")
+
+        with open('server/utils/scrapper/spider/sbu.json', 'r') as sbu_file:
+            sbu_list = json.load(sbu_file)
+
+        with open('server/utils/scrapper/spider/kbli.json', 'r') as kbli_file:
+            kbli_list = json.load(kbli_file)
+
+        syarat_kualifikasi_text = item['syarat_kualifikasi']
+        item['kode_sbu'] = next((sbu for sbu in sbu_list if sbu in syarat_kualifikasi_text), 'N/A')
+        item['kode_kbli'] = next((kbli for kbli in kbli_list if kbli in syarat_kualifikasi_text), 'N/A')
+
+        del item['syarat_kualifikasi']
+
+        # Navigate to the steps detail page
+        detail_url = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/table/tbody/tr[6]/td/a").get_attribute('href')
+        self.driver.execute_script(f"window.open('{detail_url}', '_blank');")
+        WebDriverWait(self.driver, 10).until(lambda d: len(d.window_handles) > 2)
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+
+        # Scrape the data from the table step
+        item['tahapan_tender'] = []
+        rows = self.driver.find_elements(By.XPATH, "/html/body/div[2]/div/div/table/tbody/tr")
+
+        for i, row in enumerate(rows, start=2):  # Starting from 2 to match tr[2], tr[3], etc.
+            tahap = get_text_or_default(f"/html/body/div[2]/div/div/table/tbody/tr[{i}]/td[2]", default="")
+            mulai = get_text_or_default(f"/html/body/div[2]/div/div/table/tbody/tr[{i}]/td[3]", default="")
+            sampai = get_text_or_default(f"/html/body/div[2]/div/div/table/tbody/tr[{i}]/td[4]", default="")
+    
+            if tahap or mulai or sampai:  # Only append if at least one value is not empty
+                item['tahapan_tender'].append({
+                    'tahap': tahap,
+                    'mulai': mulai,
+                    'sampai': sampai
+                })
 
         return item
 
